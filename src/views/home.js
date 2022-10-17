7a2c22059b39fc015ddd97573edfffae1a7becf5
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2022-10-17 16:11:49 +0300

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>International Applications Assistant</title>
+        <meta
+          property="og:title"
+          content="International Applications Assistant"
+        />
+      </Helmet>
+    </div>
+  )
+}
+
+export default Home
